e9f7277bb50d58b599f610438ce3ab01988078e2
AndrewZalesski <47337459+AndrewZalesski@users.noreply.github.com>
2024-08-23 22:27:55 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
+
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Define routes here
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Root route to handle '/' requests
+app.get('/', (req, res) => {
+  res.send('Welcome to Kaspa Raffles!');
+});
+
+// Catch-all route to serve React app for any other requests
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`
+   _  __           _                 _  __        __ _     
+  | |/ /          | |               | |/ /       / _| |    
+  | ' / __ _ _ __ | |__   __ _ ___  | ' /  __ _ | |_| |__  
+  |  < / _\` | '_ \| '_ \ / _\` / __| |  <  / _\` ||  _| '_ \ 
+  | . \ (_| | |_) | | | | (_| \__ \ | . \| (_| || | | |_) |
+  |_|\_\__,_| .__/|_| |_|\__,_|___/ |_|\_\__,_||_| |_.__/ 
+           | |                                           
+           |_|                                           
+  Server is running on http://localhost:${port}
+  `);
 });
